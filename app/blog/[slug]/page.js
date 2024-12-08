7181271c1cf7c337c9291b802d2c1d827f2b63d5
zarkonow <stojanov1987@gmail.com>
2024-12-08 17:38:23 +0100

    * domaci #20

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,38 +1,31 @@
+import Image from "next/image";
 
+export default async function Slug({ params }) {
+  const res = await fetch(
+    process.env.PRODUCT_API_URL + "/products/" + params.slug
+  );
 
+  const data = await res.json();
 
+  console.log(data);
 
-export default async function Slug({params}) {
-
-const res = await fetch (process.env.PRODUCT_API_URL+'/products/' +params.slug )
-
-const data = await res.json()
-
-console.log(data);
-
-
-if (res.status === 404) {
-  return('404 Not Found')
-}
-
-
+  if (res.status === 404) {
+    return "404 Not Found";
+  }
 
   return (
     <div>
-    <h1 className="text-xl font-bold">Proizvod: {params.slug} </h1>
-    
-    
-    <div>
-      
-        <p>{data.title}</p>
-      
+      <h1 className="text-xl font-bold p-2">Proizvod: {params.slug} </h1>
+
+      <div>
+        <Image src={data.thumbnail} alt={data.title} width="200" height="100" />
+        <p className="text-2xl p-2">{data.title}</p>
+        <p className="text-xs text-gray-400 text-start p-2">
+          {data.description}
+        </p>
+        <p className="text-xl p-2">{data.brand}</p>
+      </div>
     </div>
-    
-    
-    </div>
-
-
-
-  )
+  );
 }
 //20.5
--- app/page.js
+++ app/page.js
@@ -1,36 +1,34 @@
 import Image from "next/image";
 
 export default async function Home() {
-  const res = await fetch(process.env.PRODUCT_API_URL+'/products?limit=9');
+  const res = await fetch(process.env.PRODUCT_API_URL + "/products?limit=9");
   const data = await res.json();
 
   console.log(data);
 
   return (
     <div>
-<h1 className="text-4xl">Proizvodi:</h1>
+      <h1 className="text-4xl">Proizvodi:</h1>
 
       <div className="grid grid-cols-4 gap-4 ">
-
-      {data.products.map((product) => (
-        <div className="mt-3" key={product.id}>
-          <Image 
-            src={product.thumbnail}
-            alt={product.title}
-            width="100"
-            height="50"
-          />
-          <p className="text-xl line-clamp-2">{product.title}</p>
-          <p className="text-xs text-gray-400 text-start line-clamp-4">{product.description}</p>
-          <p className="text-xl line-clamp-1">{product.brand}</p>
-        </div>
-      ))}
+        {data.products.map((product) => (
+          <div className="mt-3" key={product.id}>
+            <Image
+              src={product.thumbnail}
+              alt={product.title}
+              width="100"
+              height="50"
+            />
+            <p className="text-xl line-clamp-2">{product.title}</p>
+            <p className="text-xs text-gray-400 text-start line-clamp-4">
+              {product.description}
+            </p>
+            <p className="text-xl line-clamp-1">{product.brand}</p>
+          </div>
+        ))}
       </div>
-      
-     
     </div>
   );
 }
-
 
 //21.8
